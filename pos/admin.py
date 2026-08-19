b8294755fe44e3f4260f7af52b13200dc11b6144
EmmanuelMG7 <emmamg714@gmail.com>
2026-08-19 15:42:59 -0500

    feat: diseño relacional de la BD, modelos de datos y registro en panel admin

--- pos/admin.py
+++ pos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Cliente, Empleado, Producto, Factura, DetalleFactura, Devolucion
+
+admin.site.register(Cliente)
+admin.site.register(Empleado)
+admin.site.register(Producto)
+admin.site.register(Factura)
+admin.site.register(DetalleFactura)
+admin.site.register(Devolucion)
