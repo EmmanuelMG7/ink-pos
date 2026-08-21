e97f9fe4e20c75d9a0b99ac05befe8cdfa2700d3
Juancho432 <juanmanueljaramillojaramillo4@gmail.com>
2026-08-21 11:18:52 -0500

    feat: add URL patterns and views for facturas, devoluciones, ventas, gestion_empleados, gestion_facturas, and reporte_ventas

--- pos/urls.py
+++ pos/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", view = views.Login, name = "login")
+    path("", view=views.Login, name="login"),
+    path("facturas/", view=views.facturas_view, name="facturas"),
+    path("devoluciones/", view=views.devoluciones_view, name="devoluciones"),
+    path("ventas/", view=views.ventas_view, name="ventas"),
+    path("gestion_empleados/", view=views.gestion_empleados_view, name="gestion_empleados"),
+    path("gestion_facturas/", view=views.gestion_facturas_view, name="gestion_facturas"),
+    path("reporte_ventas/", view=views.reporte_ventas_view, name="reporte_ventas"),
 ]
