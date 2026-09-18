4f9f3d4e8954b595e9764f4d403384ec6a5c668f
Juancho432 <juanmanueljaramillojaramillo4@gmail.com>
2026-09-18 18:03:26 -0500

    feat(empleados): remplazar formulario Agregar_Empleado por formulario django

--- app_empleados/views.py
+++ app_empleados/views.py
@@ -1,50 +1,34 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
+from app_empleados.forms import EmpleadoCrearForm
 from .models import Empleado
 
 
 @login_required
 def gestion_empleados_view(request):
-    if request.method == "POST":
-        if "identificacion" in request.POST:
-            identificacion = request.POST.get("identificacion")
-            nombre = request.POST.get("nombre")
-            usuario = request.POST.get("usuario")
-            telefono = request.POST.get("telefono")
-            contrasena = request.POST.get("contrasena")
-            es_admin = request.POST.get("admin_checkbox") == "true"
+    form = EmpleadoCrearForm(request.POST or None)
 
+    if request.method == "POST" and "identificacion" in request.POST:
+        if form.is_valid():
             try:
-                if User.objects.filter(username=usuario).exists():
-                    messages.error(
-                        request, f"Ya existe un empleado con el usuario '{usuario}'."
-                    )
-                else:
-                    new_user = User.objects.create_user(
-                        username=usuario, password=contrasena, first_name=nombre
-                    )
-
-                    if es_admin:
-                        new_user.is_staff = True
-                        new_user.save()
-
-                    Empleado.objects.create(
-                        id=identificacion,
-                        usuario=new_user,
-                        telefono=telefono,
-                        salario=0,
-                        es_admin=es_admin,
-                    )
-                    messages.success(
-                        request, f"Empleado '{nombre}' creado correctamente."
-                    )
+                empleado = form.save()
+                messages.success(
+                    request, f"Empleado '{empleado.usuario.first_name}' creado correctamente."
+                )
             except Exception as e:
                 messages.error(request, f"Hubo un error al crear el empleado: {str(e)}")
-
+            return redirect("empleados:gestion")
+        else:
+            for error_list in form.errors.values():
+                for err in error_list:
+                    messages.error(request, err)
             return redirect("empleados:gestion")
 
     empleados = Empleado.objects.all().order_by("-id")
-    return render(request, "empleados/Gestion_Empleados.html", {"empleados": empleados})
+    return render(
+        request,
+        "empleados/Gestion_Empleados.html",
+        {"empleados": empleados, "form": form},
+    )
