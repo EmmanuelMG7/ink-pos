3a0f9831c9a6d05ba690bf3883567edcef8d5429
Juancho432 <juanmanueljaramillojaramillo4@gmail.com>
2026-09-18 09:31:36 -0500

    feat(clientes): modularizar gestion de clientes en app independiente

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("common/", include("common.urls")),
+    path("clientes/", include("clientes.urls")),
     path("", include("pos.urls")),
 ]
