306ffee8674165a1b8fe110b560b99420be1e097
Juancho432 <juanmanueljaramillojaramillo4@gmail.com>
2026-09-22 15:55:16 -0500

    feat: registrar nuevos modelos en paginas de admin

--- app_caja/admin.py
+++ app_caja/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import SesionCaja, AjustesCaja
 
-# Register your models here.
+class AjustesCajaInline(admin.TabularInline):
+    model = AjustesCaja
+    extra = 0
+
+@admin.register(SesionCaja)
+class SesionCajaAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "empleado",
+        "estado",
+        "monto_inicial",
+        "monto_final_calculado",
+        "monto_final_real",
+        "diferencia",
+        "fecha_hora_apertura",
+        "fecha_hora_cierre",
+    )
+    list_filter = ("estado", "fecha_hora_apertura", "empleado")
+    search_fields = ("empleado__auth_user__username", "empleado__identificacion")
+    inlines = [AjustesCajaInline]
+
+@admin.register(AjustesCaja)
+class AjustesCajaAdmin(admin.ModelAdmin):
+    list_display = ("id", "sesion_caja", "tipo", "monto", "fecha_hora")
+    list_filter = ("tipo", "fecha_hora")
+    search_fields = ("motivo",)
--- app_inventario/admin.py
+++ app_inventario/admin.py
@@ -1,5 +1,49 @@
 from django.contrib import admin
+from .models import Categoria, Marca, Impuesto, Producto, MovimientoInventario
 
-from .models import Producto
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre")
+    search_fields = ("nombre",)
 
-admin.site.register(Producto)
+@admin.register(Marca)
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre")
+    search_fields = ("nombre",)
+
+@admin.register(Impuesto)
+class ImpuestoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "codigo_tributario", "tarifa", "tipo_impuesto", "activo")
+    list_filter = ("tipo_impuesto", "activo")
+    search_fields = ("nombre", "codigo_tributario")
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "nombre",
+        "marca",
+        "categoria",
+        "costo_compra",
+        "precio_venta",
+        "stock_actual",
+        "stock_minimo",
+        "activo",
+    )
+    list_filter = ("marca", "categoria", "activo")
+    search_fields = ("nombre",)
+
+@admin.register(MovimientoInventario)
+class MovimientoInventarioAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "producto",
+        "tipo_movimiento",
+        "cantidad",
+        "stock_anterior",
+        "stock_posterior",
+        "empleado",
+        "fecha_hora",
+    )
+    list_filter = ("tipo_movimiento", "fecha_hora")
+    search_fields = ("producto__nombre", "referencia_origen")
